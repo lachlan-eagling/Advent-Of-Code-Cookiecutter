debff1f0b3cc73b7ff28efd218acdc989e992ffa
Lachlan Eagling <lochie.eagling@gmail.com>
2019-11-17 06:33:24 +1000

    cleanup code generation

--- generate/main.py
+++ generate/main.py
@@ -2,43 +2,40 @@ from os import mkdir
 from os.path import basename, abspath, join, exists
 from jinja2 import Template
 
+
+def create_empty_file(filepath: str):
+    with open(filepath, "w+"):
+        return
+
 def generate_solution_files():
     for i in range(1, 26):
         answers_path = abspath(f'./advent_of_Code/data/answers')
         day_path = join(answers_path, f"day_{i}")
         if not exists(day_path):
             mkdir(day_path)
-        with open(join(answers_path, f"day_{i}", "part_1.txt"), "w+") as f:
-            pass
-
-        with open(join(answers_path, f"day_{i}", "part_2.txt"), "w+") as f:
-            pass
-        continue
+        create_empty_file(join(answers_path, f"day_{i}", "part_1.txt"))
+        create_empty_file(join(answers_path, f"day_{i}", "part_2.txt"))
 
 
 def generate_input_files():
     for i in range(1, 26):
-        with open(abspath(f'./advent_of_Code/data/input/day_{i}'), "w+") as f:
-            pass
-        continue
+        create_empty_file(abspath(f'./advent_of_Code/data/input/day_{i}'))
 
 
 def generate_test_files():
     for i in range(1, 26):
         with open(abspath(f"./generate/templates/test.tmpl")) as f:
             template = Template(f.read())
-            with open(abspath(f"./advent_of_Code/challenges/tests/test_day_{i}.py"), "w+") as f:
-                f.write(template.render(day=i))
-        continue
+        with open(abspath(f"./advent_of_Code/challenges/tests/test_day_{i}.py"), "w+") as f:
+            f.write(template.render(day=i))
 
 
 def generate_challenge_files():
     for i in range(1, 26):
         with open(abspath(f"./generate/templates/challenge.tmpl")) as f:
             template = Template(f.read())
-            with open(abspath(f"./advent_of_Code/challenges/day_{i}.py"), "w+") as f:
-                f.write(template.render(day=i))
-        continue
+        with open(abspath(f"./advent_of_Code/challenges/day_{i}.py"), "w+") as f:
+            f.write(template.render(day=i))
 
 
 def generate_all():
