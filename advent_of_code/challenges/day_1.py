91127c551600da618bb3083f12fea8950ddf6946
bennettscience <brian.bennett2@gmail.com>
2019-12-01 20:35:44 -0500

    aoc 2019 day 1

--- advent_of_code/challenges/day_1.py
+++ advent_of_code/challenges/day_1.py
@@ -1,15 +1,47 @@
 from advent_of_code.utils import load_input, save_answers
+import math
 
+# Specifically, to find the fuel required for a module, take its mass, divide by three, round down, and subtract 2.
 
-INPUT = load_input(1)
-
+modules = load_input(1)
+part1_individual = []
+part2_individual = []
 
 def part1():
-    return "Challenge not solved"
+
+    for module in modules:
+        fuel = ( math.floor( int(module) / 3 ) ) - 2
+        part1_individual.append(fuel)
+        # print(fuel)
+
+    total = sum(part1_individual)
+    return total
 
 
 def part2():
-    return "Challenge not solved"
+    # for each module mass, calculate its fuel and add it to the total
+    # treat the fuel amount you just calculated as the input mass and repeat the process, 
+    # continuing until a fuel requirement is zero or negative.
+    def check_fuel(fuel, module_total):
+        print(f'calculating fuel for volume: {fuel}')
+        volume = math.floor(fuel / 3) - 2
+        
+        if volume >= 0:
+            print(f'Adding {volume} for a total of {module_total}')
+            module_total += volume
+            print(f'Recursing for remaining {volume}')
+            check_fuel(volume, module_total)
+        else:
+            print(f'Returning {module_total}')
+            part2_individual.append(module_total)
+            return module_total
+
+    for module in modules:
+        print(f'STARTING ========================== {int(module)}')
+        fuel = check_fuel(int(module), 0)
+
+    return sum(part2_individual)
+    # return "Challenge not completed."
 
 
 if __name__ == "__main__":
--- advent_of_code/challenges/tests/test_day_1.py
+++ advent_of_code/challenges/tests/test_day_1.py
@@ -1,10 +1,10 @@
-from unittest import TestCase
+import unittest
 from advent_of_code.challenges import day_1 as challenge
 
 from advent_of_code.utils import load_answer
 
 
-class TestDay1(TestCase):
+class TestDay1(unittest.TestCase):
 
     def test_part_1(self):
         result = challenge.part1()
@@ -12,6 +12,10 @@ class TestDay1(TestCase):
         self.assertEqual(str(result), answer)
 
     def test_part_2(self):
-        result = challenge.part1()
+        result = challenge.part2()
         answer = load_answer(1, 2)
         self.assertEqual(str(result), answer)
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
